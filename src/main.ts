058912fa6a1ebeb8a563cd3847b97dd7a7295599
NinjaManatee <andy@chartiq.com>
2017-12-18 11:56:23 -0500

    Removed note because phaser.min.js will be copied

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,6 @@
 /// <reference path="../node_modules/phaser/typescript/phaser.d.ts" /> 
 
 window.onload = () => {
-
-	//  Note that this html file is set to pull down Phaser 2.5.0 from the JS Delivr CDN.
-	//  Although it will work fine with this tutorial, it"s almost certainly not the most current version.
-	//  Be sure to replace it with an updated version before you start experimenting with adding your own code.
 	const preload = () => {
 		game.load.image("logo", "img/phaser.png");
 	}
